820c94261356dca02cc582e5d79395cf43fa508e
Vr <veridetta@gmail.com>
2022-12-11 02:22:16 +0700

    Server

--- app/src/main/java/com/inc/vr/corp/app/gameepo/api/ServiceBuilder.kt
+++ app/src/main/java/com/inc/vr/corp/app/gameepo/api/ServiceBuilder.kt
@@ -10,7 +10,7 @@ object ServiceBuilder {
     var httpClient = OkHttpClient.Builder().addInterceptor(logging)
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://epogame.my.id/api/") // change this IP for testing by your actual machine IP
+        .baseUrl("http://game.epogame.my.id/api/") // change this IP for testing by your actual machine IP
         //.baseUrl("http://192.168.43.235:8000/api/")
            // .baseUrl("https://192.168.43.138:8000/api/")
         .addConverterFactory(GsonConverterFactory.create())
